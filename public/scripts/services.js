6b4a613ee485f921a0ac917751d23c8369b902bd
Tony Childs <tony.childs@gmail.com>
2013-10-03 02:28:15 +0000

    added width style

--- public/scripts/services.js
+++ public/scripts/services.js
@@ -3,8 +3,8 @@
 /* Services */
 
 angular.module('myServices', ['ngResource']).
-    factory('User', function($resource){
-  return $resource('api/users/:userId', {}, {
-    query: {method:'GET', params:{userId:''}, isArray:true}
+  factory('User', function($resource) {
+    return $resource('api/users/:userId', {}, {
+      query: {method:'GET', params:{userId:''}, isArray:true}
+    });
   });
-});
