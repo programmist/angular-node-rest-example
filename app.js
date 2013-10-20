a177522e1fe413669a471985c9c4bee3437b4e91
Tony Childs <tony.childs@gmail.com>
2013-10-20 17:00:32 +0000

    added mongo/mongoose integration

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 var express = require('express');
 var app = express();
-var user = require('./routes/user');
-var note = require('./routes/note');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://admin:pass#123@ds049848.mongolab.com:49848/example');
+var User = mongoose.model('User', { name: String, phone: String, dept: String, loc: String }, 'User');
+
+// create sample collection if not exists
+mongoose.connection.once('open', function callback () {
+  User.find(function(error, users) {
+    console.log(users.length);
+    if(users.length == 0) {
+      new User({name: "Tony", phone: "800-555-0000", dept: "ENG", loc: "E42"}).save();
+      new User({name: "Nicole",phone: "800-555-1111",dept: "BUS",loc: "B24"}).save();
+      new User({name: "Mike",phone: "800-555-2222",dept: "ACC",loc: "A01"}).save();
+    }
+  });
+});
 
 app.use(express.static(__dirname + '/public'));
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 
 app.use(app.router);
+
+var user = require('./routes/user');
 app.get('/api/users', user.list);
 app.get('/api/users/:userId', user.user);
-app.get('/api/notes', note.list);
+
+// var note = require('./routes/note');
+// app.get('/api/notes', note.list);
 
 
 app.listen(3000);
--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,16 @@
-var users = [
-    {id: 1, name: "Tony", phone: "214-555-1212", dept: "ENG", loc: "E42"},
-    {id: 2, name: "Nicole", phone: "214-555-0000", dept: "BUS", loc: "B24"},
-    {id: 3, name: "Mike", phone: "214-555-1111", dept: "ACC", loc: "A01"},
-  ]
-  
-var userIndex = {
-  1: users[0],
-  2: users[1],
-  3: users[2]
-}
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 exports.list = function (req, res) {
-    res.send(users);
+   User.find(function(error, users) {
+     res.send(users);
+   });
 };
     
 exports.user = function (req, res) {
-  user = userIndex[req.params.userId]
-  if(user) {
-    res.send(user);
-  } else {
-    res.send({});
-  }
+  User.find({_id:req.params.userId}, function(error, user) {
+    if(user) {
+      res.send(user[0]);
+    }
+   });
 };
